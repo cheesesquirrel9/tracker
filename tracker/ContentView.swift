6cf0a28190585af67c5875dc84bd7eb00f376647
andrew ellis <andrew.c.ellis207@gmail.com>
2022-02-17 10:26:35 -0800

    Initial Commit

---
+++ tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tracker
+//
+//  Created by andrew ellis on 2/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tracker/trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  trackerApp.swift
+//  tracker
+//
+//  Created by andrew ellis on 2/17/22.
+//
+
+import SwiftUI
+
+@main
+struct trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
